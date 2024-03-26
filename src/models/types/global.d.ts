ffee2cb356d7aeae047f9c422ed32420c2e99c1f
BrianEDB <bryan@grupoedb.com>
2024-03-26 16:37:50 -0500

    FEAT: type/global.d.ts file has been addend

---
+++ src/models/types/global.d.ts
@@ -0,0 +1,8 @@
+declare module "*.jpg"
+declare module "*.avif"
+declare module "*.png"
+declare module "*.svg"
+declare module "*.gif"
+declare module "*.css"
+declare module "*.scss"
+declare module "*.json"
